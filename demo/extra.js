4121315ef20183d9553d3608f7a77693cd1792ad
Michelle Bu <michelle@michellebu.com>
2013-05-14 01:00:18 -0700

    colors for chat

--- demo/extra.js
+++ demo/extra.js
@@ -1,7 +1,7 @@
 var util = {
   randomHSLColor: function(){
     var hue = Math.floor(Math.random()*361+1)
-    var output = "hsl(" + hue + ",100%,50%)"
+    var output = "hsl(" + hue + ",38%,50%)"
     return output
   }
 }
