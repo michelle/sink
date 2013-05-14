9b489ab88b55214841755019766378b8a146798e
Michelle Bu <michelle@michellebu.com>
2013-05-14 00:38:22 -0700

    extra spaces

--- demo/extra.js
+++ demo/extra.js
@@ -1,8 +1,7 @@
-var util = {  
+var util = {
   randomHSLColor: function(){
     var hue = Math.floor(Math.random()*361+1)
-    var output = "hsl(" + hue + ", 100%, 50%)"
+    var output = "hsl(" + hue + ",100%,50%)"
     return output
   }
-  
 }
